eeebfd88a188edd9761f38150311dc8eec4abeac
Keanu Lee <keanulee517@gmail.com>
2017-03-04 15:20:55 -0800

    Use bower for dependencies

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-importScripts('https://polygit.org/sw-toolbox+googlechrome+*/components/sw-toolbox/sw-toolbox.js');
+importScripts('bower_components/sw-toolbox/sw-toolbox.js');
 
 toolbox.router.get(/^https:\/\/keanulee.github.io\//, toolbox.fastest);
-toolbox.router.get(/^https:\/\/polygit.org\//, toolbox.fastest);
-toolbox.router.get(/^https:\/\/code.highcharts.com\//, toolbox.fastest);
