a89c2477dc99584c25bd9ef5454cbc3b60e7d91f
Keanu Lee <keanulee517@gmail.com>
2017-04-15 13:58:51 -0700

    Use GH pages to serve built site; SW from polymer-build

---
+++ sw-precache-config.js
@@ -0,0 +1,7 @@
+/*
+ * HACK(keanulee): This file is only needed because of
+ * https://github.com/Polymer/polymer-build/issues/186
+ */
+module.exports = {
+  replacePrefix: '.'
+};
--- sw.js
+++
@@ -1,3 +0,0 @@
-importScripts('bower_components/sw-toolbox/sw-toolbox.js');
-
-toolbox.router.get('/(.*)', toolbox.fastest);
